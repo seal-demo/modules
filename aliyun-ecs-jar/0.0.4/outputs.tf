b0c48a4f46a923cb3a6b63320a6850d4765a2e70
pengjiang80 <peng.jiang@hotmail.com>
2023-07-25 11:57:51 +0800

    update

--- aliyun-ecs-jar/0.0.4/outputs.tf
+++ aliyun-ecs-jar/0.0.4/outputs.tf
@@ -6,7 +6,3 @@ output "endpoint_webservice" {
   value = "http://${alicloud_instance.example.public_ip}:${var.service_port}"
 }
 
-output "dependsOn" {
-  value = var.dependsOn
-  description = "Service dependsOn"
-}
